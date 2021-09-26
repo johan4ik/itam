d33ac6b8f3cb8100f1e7fe0b98ab3b59bdc5a4d1
Alex <62964833+johan4ik@users.noreply.github.com>
2021-09-26 23:26:26 +0300

    Update first_4.py

--- homeworks/chapter_1/first_4.py
+++ homeworks/chapter_1/first_4.py
@@ -1,1 +1,10 @@
-
+lst = [int(i) for i in input().split()] #создаем список целых чисел
+minus,upper_eight,even=0,0,0 #счетчики
+for i in lst: #пробегаем по списку
+  if i<0:
+    minus+=1
+  if i>8:
+    upper_eight+=1
+  if i%2==0:
+    even+=1
+print(minus,upper_eight,even) # выводим количества
