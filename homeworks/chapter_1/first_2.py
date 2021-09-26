85f03e3d20f41f8f46545b31a9e2228dddb69182
Alex <62964833+johan4ik@users.noreply.github.com>
2021-09-26 23:20:27 +0300

    Update first_2.py

--- homeworks/chapter_1/first_2.py
+++ homeworks/chapter_1/first_2.py
@@ -1,1 +1,11 @@
+lst=[int(i) for i in input().split()] #генерируем список
+max=lst[0] # изначально в min и max хранится нулевой элемент списка
+min=lst[0]
 
+for i in range(1,len(lst)): #цикл от 1 элемента
+    if lst[i]>max:
+        max=lst[i]
+    if lst[i]<min:
+        min=lst[i]
+
+print(min,max)
