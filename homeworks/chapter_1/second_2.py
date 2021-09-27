a96e47b6f3797bac241de0b40c07ba6ef3ef4974
Alex <62964833+johan4ik@users.noreply.github.com>
2021-09-27 12:17:35 +0300

    Update second_2.py

--- homeworks/chapter_1/second_2.py
+++ homeworks/chapter_1/second_2.py
@@ -1,1 +1,22 @@
 
+k1,k2,ns=0,0,0
+n=int(input())
+
+for i in range(n):
+  text=input().split()
+  if text.count("True")+text.count("False")>=2: #если булевых переменных >=2
+    if text[3]=="True": #если ласт элемент = тру
+      k1+=1
+    elif text[3]=="False": #если ласт элемент = фолс
+      k2+=1
+    else: # ни то ни другое
+      ns+=1
+
+  else:
+    if text.count("True")==1 and text.count("False")==0:
+      k1+=1
+    else:
+      k2+=1
+
+
+print(k1,k2,ns)
