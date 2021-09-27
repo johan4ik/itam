b5a81a6509c480da903138d23d29c50f909f0bde
Alex <62964833+johan4ik@users.noreply.github.com>
2021-09-27 10:50:25 +0300

    Update second_1.py

--- homeworks/chapter_1/second_1.py
+++ homeworks/chapter_1/second_1.py
@@ -1,1 +1,11 @@
 
+n=int(input()) #количество строк
+k1,k2=0,0  #k1-очный формат, k2-заочный формат
+for i in range(n):
+  lst = input().split()
+  if lst[3]=="True":
+    k1+=1
+  else:
+    k2+=1
+
+print(k1,k2)
