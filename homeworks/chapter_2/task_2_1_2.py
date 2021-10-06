672f42be44ab451284e51917d97c6907b1cb1bc8
Alex <62964833+johan4ik@users.noreply.github.com>
2021-10-06 12:07:15 +0300

    Update task_2_1_2.py

--- homeworks/chapter_2/task_2_1_2.py
+++ homeworks/chapter_2/task_2_1_2.py
@@ -1,1 +1,20 @@
 
+def summation(sum):
+    lst=[int(i) for i in sum.split()]
+    sum=0
+    Maximum=max(lst)
+    for i in lst:
+        if i<0:
+            i*=-1
+            i*=2
+
+        i /= Maximum
+        sum+=i
+
+    return sum
+
+
+sum=input()
+x=summation(sum)
+x=float('{:.3f}'.format(x))
+print(x)
