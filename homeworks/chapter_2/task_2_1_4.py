0d381602d01523dfa3f51eb7c032fdbb91d5fe2b
Alex <62964833+johan4ik@users.noreply.github.com>
2021-10-06 12:51:11 +0300

    Update task_2_1_4.py

--- homeworks/chapter_2/task_2_1_4.py
+++ homeworks/chapter_2/task_2_1_4.py
@@ -1,1 +1,19 @@
-
+def debug_control(*args,**kwargs):
+    ints=0
+    strs=0
+    floats=0
+    for i in args:
+        if isinstance(i,int):
+            ints+=1
+        elif isinstance(i,float):
+            floats+=1
+        elif isinstance(i,str):
+            strs+=1
+    for i in kwargs:
+        if isinstance(i,int):
+            ints+=1
+        elif isinstance(i,float):
+            floats+=1
+        elif isinstance(i,str):
+            strs+=1
+    return f"str: {strs} int: {ints} float: {floats}"
